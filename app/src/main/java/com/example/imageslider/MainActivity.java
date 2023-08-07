a707706d72fc320d1cb12edadfbae8b7f9a7d701
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-08-07 14:11:37 +0600

    images sliding perfectly

--- app/src/main/java/com/example/imageslider/MainActivity.java
+++ app/src/main/java/com/example/imageslider/MainActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageSlider imageSlider=findViewById(R.id.imageSlider);
+        ArrayList<SlideModel> slideModels=new ArrayList<>();
+
+        slideModels.add(new SlideModel(R.drawable.beb, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.cake, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.chocolatefrostedcake, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.images, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.top, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.wdy, ScaleTypes.FIT));
+
+        imageSlider.setImageList(slideModels,ScaleTypes.FIT);
     }
 }
